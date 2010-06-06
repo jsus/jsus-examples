325bdc523837bfaeacd90024362eb5d9233bceab
Mark Abramov <markizko@gmail.com>
2010-06-06 16:30:24 +0400

    ExternalDependencies example

---
+++ ExternalDependencies/Test/Source/Widget/Widget.js
@@ -0,0 +1,20 @@
+/*
+---
+ 
+script: Widget.js
+ 
+description: An abstract class for all your widgets out there.
+ 
+license: MIT-style license
+ 
+authors:
+- Yaroslaff Fedin
+ 
+requires: 
+- Class/Class
+- Mash/Mash
+ 
+provides: [Widget]
+ 
+...
+*/
