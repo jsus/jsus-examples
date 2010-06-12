c9711dbb582a6a81ac86e19cef943bf4326d427b
Mark Abramov <markizko@gmail.com>
2010-06-12 20:00:48 +0400

    Extensions example

---
+++ Extensions/Orwik/Extensions/Class.js
@@ -0,0 +1,16 @@
+/*
+---
+
+script: Class.js
+
+description: Extension for Core/Class
+
+license: MIT-style license
+
+authors:
+- Valerio Proietti
+
+extends: "Core/Class"
+
+...
+*/
---
+++ Extensions/Output/class.js
@@ -0,0 +1,29 @@
+/*
+---
+
+script: Class.js
+
+description: Contains the Class Function for easily creating, extending, and implementing reusable Classes.
+
+license: MIT-style license.
+
+provides: [Class]
+
+...
+*/
+/*
+---
+
+script: Class.js
+
+description: Extension for Core/Class
+
+license: MIT-style license
+
+authors:
+- Valerio Proietti
+
+extends: "Core/Class"
+
+...
+*/
