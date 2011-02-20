29eb455fddd1482aa5f7e9a3c8019a978c5a2b97
Mark Abramov <markizko@gmail.com>
2011-02-20 09:51:52 -0500

    Postproc example

---
+++ Postproc/Output/package.js
@@ -0,0 +1,24 @@
+/*
+---
+
+script: Core.js
+
+description: Mootools fake core
+
+license: MIT-style license
+
+authors:
+- Valerio Proietti
+
+provides: [Core]
+
+...
+*/
+
+
+
+
+
+
+
+var incompatible = true;
---
+++ Postproc/Package/Source/Core.js
@@ -0,0 +1,28 @@
+/*
+---
+
+script: Core.js
+
+description: Mootools fake core
+
+license: MIT-style license
+
+authors:
+- Valerio Proietti
+
+provides: [Core]
+
+...
+*/
+
+
+//<ltIE8>
+var compatibleIE8 = true;
+//</ltIE8>
+
+
+//<1.2compat>
+var compatible12 = true;
+//</1.2compat>
+
+var incompatible = true;
